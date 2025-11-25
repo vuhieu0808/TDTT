049079ec8175878e351d3a47d0dde592c7ee3438
vuhieu0808 <minhhieuvutran046@gmail.com>
2025-11-25 22:20:44 +0700

    add loading component

--- frontend/src/components/auth/ProtectedRoute.tsx
+++ frontend/src/components/auth/ProtectedRoute.tsx
@@ -1,17 +1,18 @@
 import { useAuthStore } from "../../stores/useAuthStore";
 import { Navigate, Outlet } from "react-router";
+import Loading from "../Loading";
 
 const ProtectedRoute = () => {
-	console.log("ProtectedRoute rendered");
-	const { userProfile, loading } = useAuthStore();
-	if (loading) {
-		return <div>Loading...</div>;
-	}
-	if (!userProfile) {
-		return <Navigate to='/login' replace />;
-	}
+  console.log("ProtectedRoute rendered");
+  const { userProfile, loading } = useAuthStore();
+  if (loading) {
+    return <Loading />;
+  }
+  if (!userProfile) {
+    return <Navigate to="/login" replace />;
+  }
 
-	return userProfile ? <Outlet /> : <Navigate to='/login' replace />;
+  return userProfile ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
 export default ProtectedRoute;
